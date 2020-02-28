6f378cb55e3d8f94aa3cda0181895640f0bafd32
JohnathanWells <slaifir@hotmail.com>
2020-02-28 12:46:08 -0500

    Initial commit

---
+++ Assets/Nodes.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nodes : MonoBehaviour
+{
+    public GameObject[] nodes;
+
+    public static Nodes Instance;
+    
+    void Start()
+    {
+        Instance = this;
+        print(EnemySpawner.TellMeAJoke());
+    }
+}
